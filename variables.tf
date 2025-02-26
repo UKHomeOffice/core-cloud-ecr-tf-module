34886733db9d606d63376e04d8565cb84f1c0dd4
Johnny Che <114401755+chej-hod@users.noreply.github.com>
2025-02-26 17:00:59 +0000

    CCL-2122: move ecr_prefix to required variable, update examples and readme (#3)
    * CCL-2122: correct example for organisation
    
    * CCL-2122: Move prefix setting into a mandatory variable to avoid copy-paste issues

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
+variable "ecr_prefix" {
+  type        = string
+  description = "This is used to provide logical separation of ECR repositories. This will most likely be the name of the tenant or team"
+}
+
 variable "ecr_config" {
   type        = any
-  description = "PAth to YAML file that contains ECR repositories"
+  description = "Path to YAML file that contains ECR repositories"
 }
 
 variable "tags" {
