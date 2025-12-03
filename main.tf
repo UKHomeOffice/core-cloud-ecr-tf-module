1694c8f3f76846dac9e8dc5f24b4591b5c208830
Johnny Che <johnny.che@capgemini.com>
2025-12-03 15:10:29 +0000

    CC-614: correct variable map reference

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ module "ecr" {
 
   create_lifecycle_policy = try(each.value.create_lifecycle_policy, var.ecr_config.common_options.create_lifecycle_policy, false)
 
-  repository_read_access_arns        = try(each.value.repository_read_write_access_arns, var.ecr_config.common_options.repository_read_write_access_arns, [])
-  repository_read_write_access_arns  = try(each.value.repository_read_access_arns, var.ecr_config.common_options.repository_read_access_arns, [])
+  repository_read_access_arns        = try(each.value.repository_read_access_arns, var.ecr_config.common_options.repository_read_access_arns, [])
+  repository_read_write_access_arns  = try(each.value.repository_read_write_access_arns, var.ecr_config.common_options.repository_read_write_access_arns, [])
   repository_lambda_read_access_arns = try(each.value.repository_lambda_read_access_arns, []) # Lambda ECR access to be done on a repo by repo basis only
   repository_policy_statements       = try(each.value.repository_policy_statements, var.ecr_config.common_options.repository_policy_statements, {})
   repository_lifecycle_policy        = try(file(each.value.repository_lifecycle_policy), file(var.ecr_config.common_options.repository_lifecycle_policy), null)
